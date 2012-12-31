e051784de1874e427427185e03db05fc8ed199de
Nickfost <me@nickfost.com>
2012-12-30 20:26:37 -0600

    Finished up
    Its public on http://mcscreenshot.net

--- Index.php
+++ Index.php
@@ -1,9 +1,37 @@
 <html>
 	<head>
-		<link rel="stylesheet" type="include/style.css" href="mystyle.css">
-		<title>
-		</title>
-	</head>
-	<body>
+		<title>Latest #MCScreenshot </title>
+		<script src="include/jquery.min.js"></script>
+    <style type="text/css">
+      body {
+      }
+      .cameron {
+        font-family: sans-serif;
+        margin-top:50px;
+		margin-left:auto;
+		margin-right:auto;
+      }
+      .small {
+        font-family: sans-serif;
+        font-size: 0.8em;
+        margin:0px auto;
+        max-width:500px;
+        margin-top:50px;
+      }
+    </style>
+  </head>
+  <body id="content">
+  <center><div id="Title" class="cameron" style="font-size:4em; padding-bottom:50px;">Latest #MCSreenshot</div><center>
+  <script type="text/javascript">
+    var id = 0;
+    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23mcscreenshot&pic.twitter.com'";
+    $.getJSON(url, function(data) {
+      id = data.max_id_str;
+      var url = "https://api.twitter.com/1/statuses/oembed.json?callback=?&id="+id+"&align=center&maxwidth=900&hide_media=false&lang=en";
+      $.getJSON(url, function(data) {
+        $('#Title').after(data.html);
+      });
+    });
+    </script>
 	</body>
 </html>
