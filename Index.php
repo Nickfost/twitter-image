eba18680b0f075fa55e73cbec00206c916b84c5e
Nickfost <me@nickfost.com>
2012-12-30 22:36:22 -0600

    Configable subject

--- Index.php
+++ Index.php
@@ -1,6 +1,17 @@
+<?php
+	////////////
+	// CONFIG //
+	////////////
+	
+	$twitter_images_config = array();
+	// Subject or hashtag you can put a word here or a hashtag (defualt:#puppies)
+	$twitter_images_config['subject'] = '#puppies';
+	
+
+?>
 <html>
 	<head>
-		<title>Latest #MCScreenshot </title>
+		<title>Latest <?php $twitter_images_config['subject'] ?> </title>
 		<script src="include/jquery.min.js"></script>
     <style type="text/css">
       body {
@@ -21,10 +32,10 @@
     </style>
   </head>
   <body id="content">
-  <center><div id="Title" class="cameron" style="font-size:4em; padding-bottom:50px;">Latest #MCSreenshot</div><center>
+  <center><div id="Title" class="cameron" style="font-size:4em; padding-bottom:50px;">Latest <?php $twitter_images_config['subject'] ?></div><center>
   <script type="text/javascript">
     var id = 0;
-    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23mcscreenshot&pic.twitter.com'";
+    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23mcscreenshot pic.twitter.com'";
     $.getJSON(url, function(data) {
       id = data.max_id_str;
       var url = "https://api.twitter.com/1/statuses/oembed.json?callback=?&id="+id+"&align=center&maxwidth=900&hide_media=false&lang=en";
