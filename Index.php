b4459ef43bdcb6f76c50fce4c5fc462ef4b4501d
Nickfost <me@nickfost.com>
2012-12-30 23:22:06 -0600

    Fully functional with topic and picture filter

--- Index.php
+++ Index.php
@@ -4,12 +4,14 @@
 	////////////
 	
 	$twitter_images_config = array();
-	// Hashtag you can put a any hashtag here (defualt:puppies)
-	$twitter_images_config['subject'] = 'puppies';
+	// Hashtag you can put a any hashtag here (defualt: Puppies)
+	$twitter_images_config['subject'] = 'Puppies';
 	// Only display images (defualt: true)
 	$twitter_images_config['images_only'] = true;
-	// Number of Tweets to display
 	
+	
+	// Number of Tweets to display (defualt: 1)
+	// DOEST NOT WORK YET  $twitter_images_config['tweet_num'] = '1';
 	
 	///////////        ////////////////
 	// STUFF //        // DONT TOUCH //
@@ -23,7 +25,7 @@
     <style type="text/css">
       body {
       }
-      .cameron {
+      .TitleClass {
         font-family: sans-serif;
         margin-top:50px;
 		margin-left:auto;
@@ -39,21 +41,13 @@
     </style>
   </head>
   <body id="content">
+  <center><div id='Title' class='TitleClass' style='font-size:4em; padding-bottom:50px;'>Latest #<?php echo $twitter_images_config['subject'] ?></div><center>
+    <script type="text/javascript">
+    var id = 0;  
 <?php
-	if ($twitter_images_config['images_only'] = true ) {
-		echo "  <center><div id='Title' class='cameron' style='font-size:4em; padding-bottom:50px;'>Latest #";
-		echo $twitter_images_config['subject'];
-		echo" pic.twitter.com</div><center>";
-	}
-	else {
-		echo "  <center><div id='Title' class='cameron' style='font-size:4em; padding-bottom:50px;'>Latest #";
-		echo $twitter_images_config['subject'];
-		echo "</div><center>";
-	}
+	if ($twitter_images_config['images_only'] = true ) { ?>    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23<?php echo $twitter_images_config['subject']; ?> pic.twitter.com'"; <?php }
+	else { ?>    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23<?php echo $twitter_images_config['subject']; ?>'"; <?php }
 ?>
-  <script type="text/javascript">
-    var id = 0;
-    var url = "http://search.twitter.com/search.json?callback=?&rpp=1&q='%23<?php echo $twitter_images_config['subject']; ?> pic.twitter.com'";
     $.getJSON(url, function(data) {
       id = data.max_id_str;
       var url = "https://api.twitter.com/1/statuses/oembed.json?callback=?&id="+id+"&align=center&maxwidth=900&hide_media=false&lang=en";
